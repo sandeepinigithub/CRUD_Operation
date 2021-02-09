2d9e0852a89ac2a565c1a5c1e0a3673b8b8738af
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-09 23:29:17 +0530

    C Operation Completed

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,9 +1,38 @@
-const express = require('express');
+const express = require("express");
 var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
 
-router.get("/" , (req,res)=>{
-    res.render("employee/addOrEdit", {
-        viewTitle : "Insert Employee"
-    });
+router.get('/', (req, res) => {
+  res.render("employee/addOrEdit", {
+    viewTitle: "Insert Employee",
+  });
 });
+router.post('/', (req, res) => {
+  //console.log(req.body);
+  insertRecord(req, res)   
+});
+
+function insertRecord(req,res){
+    var employee = new Employee();
+    employee.fullName = req.body.fullName ;
+    employee.email = req.body.email ;
+    employee.mobile = req.body.mobile ;
+    employee.city = req.body.city ;
+    employee.save((err,doc)=>{
+        if(!err){
+            res.redirect('employee/list');
+            console.log('Data inserted');
+        }
+        else{
+            console.log('Error during record insertion : ' + err);
+        }
+    });
+
+}
+
+router.get('/list', (req, res) =>{
+    res.json('from list');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,16 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
 
 
 const employeeController = require('./controllers/employeeController');
 
 var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(bodyparser.json());
 
 app.set('views' , path.join(__dirname,'/views/'));
 app.engine('hbs', exphbs({extname : 'hbs' , defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts'}));
