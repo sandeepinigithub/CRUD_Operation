3d1d7ca9e87b266c6698632ddbc8a5937104ea45
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-02-09 19:29:34 +0530

    views and form created

---
+++ controllers/employeeController.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var router = express.Router();
+
+router.get("/" , (req,res)=>{
+    res.render("employee/addOrEdit", {
+        viewTitle : "Insert Employee"
+    });
+});
+module.exports = router;
